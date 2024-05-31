9542d3ce4fd07cd27cb3dae9c8250d921f8c06b7
otegami <a.s.takuya1026@gmail.com>
2024-05-31 10:52:34 +0900

    Use systemctl is-system-running --wait

--- test.sh
+++ test.sh
@@ -1,16 +1,8 @@
 sudo incus admin init --auto
 sudo incus launch images:almalinux/8 target
-max_retry=3
-retry=0
-until sudo incus exec target -- sudo dnf update -y; do
-  retry=$((retry + 1))
-  echo "Attempt $retry of $max_retry"
-  if [ $retry -ge $max_retry ]; then
-    echo "Network is not ready after $max_retry attempts. Exiting."
-    exit 1
-  fi
-  echo "Waiting for network to be ready... retrying in 3 second."
-  sleep 3
+until sudo incus exec target -- sudo systemctl is-system-running --wait; do
+  sleep 1
 done
+sudo incus exec target -- sudo dnf update -y
 sudo incus stop target
 sudo incus delete target
