e3c47689882379dc15e5445cf852ae049f0d271f
otegami <a.s.takuya1026@gmail.com>
2024-06-01 17:30:15 +0800

    Conclusion

--- test.sh
+++
@@ -1,8 +0,0 @@
-sudo incus admin init --auto
-sudo incus launch images:almalinux/8 target
-until sudo incus exec target -- sudo systemctl is-system-running --wait; do
-  sleep 1
-done
-sudo incus exec target -- sudo dnf update -y
-sudo incus stop target
-sudo incus delete target
